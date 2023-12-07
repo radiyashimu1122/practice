ae14e50e093896521a32093b00b61c320a9d6030
radiyashimu1122 <radiyashimu@gmail.com>
2023-12-07 06:46:16 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var age = 30;
+// console.log(typeof age);
+var status =true;
+// console.log (typeof status);
+const num1 =30.555;
+// console.log( parseInt (num1));
+const num2 =30.5;
+const num3 =15.5;
+const num4 = parseInt (num2-num3);
+// console.log(num4);
+
