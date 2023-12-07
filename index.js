f8da468d7b634dbe61964045514aea1a2a5d6dc5
radiyashimu1122 <radiyashimu@gmail.com>
2023-12-07 07:42:20 -0800

    second practice

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ const num2 =30.5;
 const num3 =15.5;
 const num4 = parseInt (num2-num3);
 // console.log(num4);
+var age = 50;
+if (age>=50 ) {
+ console.log("Tumi ekjon briddho manush")
+}
+else if (age<18) {
+  console.log("Tumi baccha manush");
+}
+else {
+console.log("Error")
+}
 
+ 
